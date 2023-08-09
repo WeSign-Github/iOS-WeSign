d1b28943df3f683589c8e223ae67797411320022
HafizhS <methafizh30@gmail.com>
2023-08-10 01:33:54 +0700

    Initial Commit

---
+++ WeSign/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeSign
+//
+//  Created by Hafizh Sumantri on 10/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSign/WeSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSignApp.swift
+//  WeSign
+//
+//  Created by Hafizh Sumantri on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
